a5374d79541a8b63495f00f02d3915a01f1fae0f
q400400200-pix <q.400400200@gmail.com>
2026-04-30 11:55:59 +0000

    add

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,41 +1,20 @@
 #!/usr/bin/python3
-""" FizzBuzz
-"""
 import sys
 
-
 def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
-    if n < 1:
-        return
-
-    tmp_result = []
+    """يطبع الأرقام من 1 إلى n مع استبدال مضاعفات 3 و 5"""
+    result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-                   
-        elif (i % 5) != 0 and ( i % 3) == 0:
-        # elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0 and ( i % 3) != 0:
-            tmp_result.append("Buzz")
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+            result.append(str(i))
+    print(" ".join(result))
 
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        fizzbuzz(int(sys.argv[1]))
