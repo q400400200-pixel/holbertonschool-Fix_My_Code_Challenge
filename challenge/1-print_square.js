1f414d66a34ce82ea3ba1fc7b62f90ca495e977a
q400400200-pix <q.400400200@gmail.com>
2026-04-30 11:59:44 +0000

    add

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,25 +1,10 @@
 #!/usr/bin/node
-/*
-    Print a square with the character #
-    
-    The size of the square must be the first argument 
-    of the program.
-*/
+const size = process.argv[2];
 
-
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+if (size === undefined || isNaN(size)) {
+  process.exit();
 }
 
-// size = parseInt(process.argv[2], 16)
-size = parseInt(process.argv[2], 10)
-
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+for (let i = 0; i < size; i++) {
+  console.log("#".repeat(size));
 }
